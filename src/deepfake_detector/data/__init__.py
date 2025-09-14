49ccb05dfce6cb8bb066709fe4ae4bb8d5877650
Bivek Sharma Panthi <bivek200312@unm.edu>
2025-09-14 04:10:17 -0600

    updated init file

--- src/deepfake_detector/data/__init__.py
+++ src/deepfake_detector/data/__init__.py
@@ -11,9 +11,41 @@ Author: Bivek Sharma Panthi
 """
 
 from .dataset_manager import DatasetManager, DatasetRegistry, DatasetInfo
+from .video_processor import VideoProcessor, FaceDetector, VideoMetadata, FaceDetection
+from .audio_processor import AudioProcessor, AudioMetadata, AudioFeatures
+from .data_pipeline import (
+    DeepfakeVideoDataset,
+    DeepfakeAudioDataset,
+    DataSplitter,
+    DataSplit,
+    VideoAugmentations,
+    AudioAugmentations,
+    DataPipelineManager
+)
 
 __all__ = [
+    # Dataset management
     "DatasetManager",
     "DatasetRegistry", 
     "DatasetInfo",
+    
+    # Video processing
+    "VideoProcessor",
+    "FaceDetector",
+    "VideoMetadata",
+    "FaceDetection",
+    
+    # Audio processing
+    "AudioProcessor",
+    "AudioMetadata", 
+    "AudioFeatures",
+    
+    # PyTorch pipeline
+    "DeepfakeVideoDataset",
+    "DeepfakeAudioDataset",
+    "DataSplitter",
+    "DataSplit",
+    "VideoAugmentations",
+    "AudioAugmentations", 
+    "DataPipelineManager",
 ]
