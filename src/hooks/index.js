5a8b9290e5060f4b094075e0ec265cf9cf48e25d
Bivek Sharma Panthi <bivek200312@unm.edu>
2025-10-10 22:02:53 -0600

    feat: add keyboard shortcuts (U=upload, W=webcam, R=reset, Esc=stop)

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -2,3 +2,4 @@ export { useWebcam } from './useWebcam';
 export { usePrediction } from './usePrediction';
 export { useFileUpload } from './useFileUpload';
 export { useMode } from './useMode';
+export { useKeyboardShortcuts } from './useKeyboardShortcuts';
