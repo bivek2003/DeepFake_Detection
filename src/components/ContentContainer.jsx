8f53700c85d5f58fe3c974a06e527d823cd68f01
Bivek Sharma Panthi <bivek200312@unm.edu>
2025-10-10 23:24:57 -0600

    Move frontend project into deepfake-frontend folder

--- src/components/ContentContainer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const ContentContainer = ({ children, className = '' }) => {
-  return (
-    <div className={`bg-slate-800/80 backdrop-blur-sm rounded-xl shadow-2xl p-8 mb-8 border border-slate-700 ${className}`}>
-      {children}
-    </div>
-  );
-};
